28e4175378c87e7aba224fc74d758a397a468a9d
bidaAnastasiia <bida.anastasiia@gmail.com>
2021-04-17 19:03:13 +0200

    task 1.5

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ async def show_methodDel(request: Request):
 async def show_methodOpt(request: Request):
     return {"method": request.method}
 
+
 @app.get("/auth", status_code=status.HTTP_401_UNAUTHORIZED)
 async def auth_method(password: str = "", password_hash: str = ""):
     statusCode = status.HTTP_401_UNAUTHORIZED
@@ -51,6 +52,7 @@ class User(BaseModel):
 
 
 i = 0
+patient_list = []
 
 
 @app.post("/register", status_code=status.HTTP_201_CREATED)
@@ -61,6 +63,20 @@ async def register_method(user: User):
     bufsurname = ''.join(symbol for symbol in user.surname if symbol.isalpha())
     print(bufname + bufsurname)
     delta = timedelta(len(bufname + bufsurname))
-    return {"id": i, "name": user.name, "surname": user.surname,
-            "register_date": date.today().strftime("%Y-%m-%d"),
-            "vaccination_date": date.today()+delta}
+    patient = {"id": i, "name": user.name, "surname": user.surname,
+               "register_date": date.today().strftime("%Y-%m-%d"),
+               "vaccination_date": date.today() + delta}
+    global patient_list
+    patient_list.append(patient)
+    return patient
+
+
+@app.get("/patient/{id}")
+async def get_patient(id: int):
+    if id < 1:
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+    global patient_list
+    if id >= len(patient_list):
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+    else:
+        return patient_list[id]
