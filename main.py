1ae824466ab6e4b2bfe34816518b6481f3277229
bidaAnastasiia <bida.anastasiia@gmail.com>
2021-04-17 19:10:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ async def get_patient(id: int):
     if id < 1:
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
     global patient_list
-    if id >= len(patient_list):
+    if id > len(patient_list):
         return Response(status_code=status.HTTP_404_NOT_FOUND)
     else:
         return patient_list[id-1]
